9951c57759625dae9afdbc0058c0bd101522bf67
gizmore <gizmore@wechall.net>
2018-03-13 20:40:26 +0100

    6.07_Various

--- tpl/listitem/link.php
+++ tpl/listitem/link.php
@@ -1,12 +1,12 @@
 <?php /** @var $link \GDO\Links\GDO_Link **/
 use GDO\Date\Time;
 use GDO\UI\GDT_Link;
+use GDO\UI\GDT_Menu;
 use GDO\User\GDO_User;
 use GDO\Links\GDT_LinkTitle;
-use GDO\Avatar\GDO_Avatar;
-use GDO\User\GDT_LevelPopup;
-use GDO\Vote\GDT_VotePopup;
-
+use GDO\Profile\GDT_ProfileLink;
+use GDO\Vote\GDT_VoteSelection;
+use GDO\Vote\GDT_VoteRating;
 $creator = $link->getCreator();
 $user = GDO_User::current();
 $username = $creator->displayNameLabel();
@@ -15,15 +15,21 @@ $age = Time::displayAge($date);
 $views = $link->getViews();
 $votes = $link->getVoteCount();
 $rating = $link->getVoteRating(); ?>
-<md-list-item class="md-3-line">
-  <?= GDO_Avatar::renderAvatar($creator); ?>
-  <div class="md-list-item-text" layout="column">
-    <h3><?= GDT_LinkTitle::make()->gdo($link)->render(); ?></h3>
-    <h4><?= $creator->displayName(); ?></h4>
-    <p>
-      <?= GDT_LevelPopup::make()->level($link->getLevel())->render(); ?>
-      <?= GDT_VotePopup::make()->gdo($link)->render(); ?>
-    </p>
+<div class="gdt-list-item">
+  <div><?=GDT_ProfileLink::make()->forUser($creator)->render()?></div>
+  <div class="gdt-content">
+    <h3><?=GDT_LinkTitle::make()->gdo($link)->render()?></h3>
+    <h4>
+      <?=GDT_VoteRating::make()->gdo($link)->render()?>
+      <?=GDT_VoteSelection::make()->gdo($link)->renderForm()?>
+    </h4>
+    <p><?=$link->display('link_description')?></p>
   </div>
-  <?= GDT_Link::make('link_view')->href($link->href_visit())->render(); ?>
-</md-list-item>
+  <div class="gdt-actions">
+<?php
+$menu = GDT_Menu::make();
+$menu->addField(GDT_Link::make('link_view')->href($link->href_visit()));
+echo $menu->render();
+?>
+  </div>
+</div>
